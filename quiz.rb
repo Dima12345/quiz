29dab4b183618de5d4473824e9d8c98ab934f52e
dima12345 <gunosada1@gmail.com>
2017-07-06 15:39:51 +0300

    Scrape https://staqresults.herokuapp.com page

--- quiz.rb
+++ quiz.rb
@@ -3,6 +3,34 @@ require 'nokogiri'
 
 class Quiz
   def run
+    html_page = open_page
+    headers = html_page.css('thead').css('th').to_a
+    values = html_page.css('tbody').css('tr')
+    answer = Hash.new
+    values.each do |tr|
+      key = String.new
+      value = Hash.new
+      tr.css('td').to_a.each_index do |index|
+        if index == 0
+          key = tr.css('td')[index].text
+        else
+          value.merge!({ headers[index].text.downcase.to_sym => tr.css('td')[index].text })
+        end
+        answer.merge!({ key => value })
+      end
+    end
+    answer
+  end
 
+  private
+
+  def open_page
+    agent = Mechanize.new
+    page = agent.get('https://staqresults.herokuapp.com')
+    form = page.form
+    form.email = 'test@example.com'
+    form.password = 'secret'
+    page = agent.submit(form)
+    html_page = Nokogiri::HTML(page.content)
   end
 end
